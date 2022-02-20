c2b8b7aac0cdcf86c3958b469e5656919071b98b
Kerwin <Kerwin@mzb.com>
2022-02-20 20:08:47 +0800

    first commit

---
+++ src/main/java/com/ma/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.ma.git;
+
+/**
+ * @Author:Kerwin
+ * @Data: 2022/2/20 - 2022 - 02 - 20 - 20:04
+ * @Description: com.ma.git
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
