1370b87e0fb99f14d6071668cf48e32d41d061b7
Kerwin <93593877+mzzbbb@users.noreply.github.com>
2022-02-20 22:18:21 +0800

    final

--- src/main/java/com/ma/git/GitTest.java
+++ src/main/java/com/ma/git/GitTest.java
@@ -16,5 +16,9 @@ public class GitTest {
         System.out.println("hot-fix test !");
         System.out.println("push test !");
         System.out.println("pull test !");
+         System.out.println("hello git !");
+         System.out.println("hello git !"); 
+        System.out.println("hello git !");
+         System.out.println("hello git !");
     }
 }
